cd02e6e3f81331d5be786fe6afe2badc05925115
Mikhail Gashenko <dr.psycho@live.com>
2018-09-08 19:17:19 +0600

    Displaying Data

---
+++ src/WeatherProject.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import Forecast from './Forecast';
+
+export default class WeatherProject extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {zip: ' ', forecast: null}
+  }
+
+  render() {
+    let content = null;
+    if (this.state.forecast !== null) {
+      content = (
+          <Forecast
+            main={this.state.forecast.main}
+            description={this.state.forecast.description}
+            temp={this.state.forecast.temp}
+          />
+      )
+    }
+
+    return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Your input: {this.state.zip}
+          </Text>
+          {content}
+          <TextInput style={styles.input}
+          onSubmitEditing={this._handleTextChange} />
+        </View>
+    )
+  }
+
+  _handleTextChange = event => {
+    this.setState({ zip: event.nativeEvent.text })
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10 },
+
+  input: {
+    fontSize: 20,
+    borderWidth: 2,
+    padding: 2,
+    height: 40,
+    width: 100,
+    textAlign: "center"
+  }
+});
